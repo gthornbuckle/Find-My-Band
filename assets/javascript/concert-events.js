d25bb96445296023ea33f11d681b72fcbe0d7d85
gthornbuckle <gthornbuckle@gmail.com>
2023-05-05 23:37:44 +0100

    Files from group repo

---
+++ assets/javascript/concert-events.js
@@ -0,0 +1,193 @@
+// Artists Events Tracker --------------------------------------------------------------------------------
+var validCountryVenuesData = []; //Empty array to store relevant API data temporarily based on user input
+
+generateConcertData = function(){
+    console.log("Finding artist information");
+    
+    // var userArtistInputNoSpace = userInputs[0].split(' ').join('%20'); //Replaces spaces with %20 (required for API call url)
+    // var userCountryInput = userInputs[2]; //Takes user inputted country
+
+    //---------TEST DATA--------------
+    
+    var response = concertEventsTrackerTestResponse;
+    var userCountryInput = "New Zealand";
+
+    //---------TEST DATA--------------
+
+    // const settings = {
+    //     "async": true,
+    //     "crossDomain": true,
+    //     "url": "https://concerts-artists-events-tracker.p.rapidapi.com/artist?name="+userArtistInputNoSpace+"&page=1",
+    //     "method": "GET",
+    //     "headers": {
+    //         "X-RapidAPI-Key": concertEventsTrackerKey,
+    //         "X-RapidAPI-Host": "concerts-artists-events-tracker.p.rapidapi.com"
+    //     }
+    // };
+    
+    // $.ajax(settings).done(function (response) {
+    //     console.log(response);
+        if(response.data === undefined || response.data.length === 0){ //Checks if artist exsits
+            console.log("Artist not found");
+    
+            // --- Write code here to display artist not found modal ---
+    
+        }
+        else{
+            var tempArtistPerformanceData = []; //Temporary array for artist venues
+            var tempArtistInfoData = {}; //Temporary Object for artist data
+    
+            tempArtistInfoData["artist"] = response.data[0].name;
+            tempArtistInfoData["image"] = response.data[0].image;
+    
+            var artistBio = $("<div>").attr("class", "artistBio"); //Creates artist bio using artist name and image
+            artistBio.css("background-image", "url("+ response.data[0].image+ ")");
+            artistName = $("<h2>").text(response.data[0].name);
+
+            console.log(tempArtistInfoData);
+            $("#artist-bio").append(artistBio);
+    
+            for (var i = 0; i < response.data.length; i++){
+                var tempDataObject = {}; //Temporary Object for venue data
+        
+                tempDataObject["description"] = response.data[i].description;
+                tempDataObject["venue"] = response.data[i].location.name;
+                tempDataObject["address"] = response.data[i].location.address;
+                tempDataObject["latitude"] = response.data[i].location.geo.latitude;
+                tempDataObject["longitude"] = response.data[i].location.geo.longitude;
+                tempDataObject["startdate"] = response.data[i].startDate;
+        
+                tempArtistPerformanceData.push(tempDataObject); //Adds temp object to temp array
+            }
+    
+            console.log(tempArtistPerformanceData);
+            validateCountryInput(tempArtistPerformanceData, userCountryInput); //Checks if the venue data contains user inputted country
+            getRequiredVenues(tempArtistPerformanceData, userCountryInput); //Creates new array containing only instances of venues in user's inputted country
+    
+            console.log(validCountryVenuesData);
+    
+            for (var i = 0; i < validCountryVenuesData.length; i++){ //Loops through tempt data and creates a card for each venue
+         
+                var eventCarousel = $("<div>").attr("class", "carousel-item");
+                var eventCard = $("<div>").attr("class", "card");
+                var eventCardContentMain = $("<div>").attr("class", "card-body");
+                var eventCardContentButton = $("<div>").attr("class", "card-body btn-toolbar");
+                var venueName = $("<h2>").text(validCountryVenuesData[i].venue);
+                var eventDate = $("<h2>").text(validCountryVenuesData[i].startdate);
+                var address = $("<p>").text("Located at: "+
+                    validCountryVenuesData[i].address.streetAddress+ " "+
+                    validCountryVenuesData[i].address.addressLocality+ ", "+
+                    validCountryVenuesData[i].address.postalCode+ ", "+
+                    validCountryVenuesData[i].address.addressCountry);
+                var description = $("<p>").text(validCountryVenuesData[i].description);
+                var viewDirections = $("<a>").attr({
+                    class: "btn btn-primary",
+                    "data-id": i+ "-directions"});
+                viewDirections.text("Directions");
+                var saveEvent = $("<a>").attr({
+                    class: "btn btn-primary",
+                    "data-id": i+ "-saveEvent"});
+                saveEvent.text("Save");
+    
+                eventCardContentMain.append(venueName);
+                eventCardContentMain.append(eventDate);
+                eventCardContentMain.append(description);
+                eventCardContentMain.append(address);
+                eventCardContentButton.append(viewDirections);
+                eventCardContentButton.append(saveEvent);
+                eventCard.append(eventCardContentMain);
+                eventCard.append(eventCardContentButton);
+                eventCarousel.append(eventCard);
+    
+                $("#events-carousel").append(eventCarousel); //Cards are appended to carousel
+                $('.carousel-item').first().addClass('active'); //Sets first card to be active to enable the carousel to work
+            }
+
+            $("#main-search-box").addClass("hide");
+            $("#results").removeClass("hide");
+    
+            $("a").on("click", function(){
+                var clickBtn = $(this).data('id'); //Returns ID of event card clicked
+
+                let saveBtn = clickBtn.includes('saveEvent'); //Checks if clicked button is for sve or directions
+                let directionBtn = clickBtn.includes('directions');
+
+                if (saveBtn === true) { 
+                    console.log("Save button clicked");
+                    saveIndex = parseInt(clickBtn); //Takes Event ID from button
+                    saveEventLocal(validCountryVenuesData[saveIndex]); //Runs save function
+                }
+                if (directionBtn === true) {
+                    console.log("Directions button clicked");
+                    calcDistanceAndTime();
+                }
+                
+            });
+        }
+    // });
+}
+
+validateCountryInput = function(arr, str){
+    const i = arr.findIndex(e => e.address.addressCountry === str);
+    if (i > -1) {
+        console.log("Reponse contains inputted country at index "+ i);
+    }
+    else{
+        // --- Write code here to display artist not performing in country found modal ---
+    }
+}
+
+getRequiredVenues = function(arr, str){
+    validCountryVenuesData = arr.filter(function(e){
+        return e.address.addressCountry === str;
+    })
+}
+
+saveEventLocal = function(obj){
+    savedEvents.push(obj); //Pushes object to local storage array
+    localStorage.setItem("savedEvents", JSON.stringify(savedEvents)); //Saves to local storage
+    console.log("Event saved");
+}
+
+displaySavedEvents = function(){
+    if (savedEvents === undefined || savedEvents.length == 0){
+        $("#placeholder").removeClass("hide");
+        console.log("No saved events found");
+    }
+    else{
+        $("#placeholder").attr("class", "hide");
+
+        for (var i = 0; i < savedEvents.length; i++){ //Loops through tempt data and creates a card for each venue
+    
+            var eventCard = $("<div>").attr("class", "card");
+            var eventCardContentMain = $("<div>").attr("class", "card-body");
+            var eventCardContentButton = $("<div>").attr("class", "card-body btn-toolbar");
+            var venueName = $("<h2>").text(savedEvents[i].venue);
+            var eventDate = $("<h2>").text(savedEvents[i].startdate);
+            var address = $("<p>").text("Located at: "+
+                savedEvents[i].address.streetAddress+ " "+
+                savedEvents[i].address.addressLocality+ ", "+
+                savedEvents[i].address.postalCode+ ", "+
+                savedEvents[i].address.addressCountry);
+            var description = $("<p>").text(savedEvents[i].description);
+            var viewDirections = $("<a>").attr({
+                class: "btn btn-primary",
+                "data-id": i+ "-direction"});
+            viewDirections.text("Directions");
+            var saveEvent = $("<a>").attr({
+                class: "btn btn-primary",
+                "data-id": i+ "-deleteEvent"});
+            saveEvent.text("Delete");
+    
+            eventCardContentMain.append(venueName);
+            eventCardContentMain.append(eventDate);
+            eventCardContentMain.append(description);
+            eventCardContentMain.append(address);
+            eventCardContentMain.append(viewDirections);
+            eventCardContentMain.append(saveEvent);
+            eventCard.append(eventCardContentMain);
+    
+            $("#saved-events").append(eventCard);
+        }
+    }
+}
---
+++ assets/javascript/find-my-band.js
@@ -0,0 +1,63 @@
+
+var savedEvents = JSON.parse(localStorage.getItem("savedEvents")) || [];
+var userInputs = [];
+
+$("#search-button").on("click", function(event){
+    event.preventDefault();
+    var userArtistInput = $("#artist-name-search").val();
+    var userAddress = $("#address-search").val();
+    var userCountryInput = $("#country-search").val();
+    
+    userInputs = [userArtistInput, userAddress, userCountryInput]; //Use this array to get any of the user inputs where needed
+
+    console.log(userInputs);
+
+    generateConcertData();
+});
+
+if (savedEvents === undefined || savedEvents.length == 0){ //Checks if any saved event data exists in local storage
+    console.log("No saved events found.");
+}
+
+if (savedEvents === undefined || savedEvents.length == 0){ //Checks if any saved event data exists in local storage
+    console.log("No saved events found.");
+}
+
+$("#saved-events-button").on("click", function(){
+    location.href = "saved-events.html"
+});
+
+
+if($("body").is(".saved-event-page")){
+    displaySavedEvents();
+    console.log("Loaded saved events");
+
+    $("#back-button").on("click", function(){
+        location.href = "index.html"
+    });
+}
+
+$("a").on("click", function(){
+    var clickBtn = $(this).data('id'); //Returns ID of event card clicked
+
+    var deleteBtn = clickBtn.includes('deleteEvent'); //Checks if clicked button is for sve or directions
+    var directionBtn = clickBtn.includes('directions');
+
+    if (deleteBtn === true) { 
+        console.log("Delete button clicked");
+        deleteIndex = parseInt(clickBtn); //Takes Event ID from button
+        savedEvents.splice(deleteIndex, 1);
+        localStorage.setItem("savedEvents", JSON.stringify(savedEvents));
+        $("#saved-events").empty();
+        displaySavedEvents();
+    }
+
+    if (directionBtn === true) {
+        console.log("Directions button clicked");
+    }
+});
+
+$("#back-button-main").on("click", function(){
+    $("#results").attr("class", "hide");
+    $("#main-search-box").removeClass("hide");
+});
